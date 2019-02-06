ceba660d981cf4399426ba7fb6e70b94aecb5c53
ArtemHorobei <artemgorobey@gmail.com>
2019-02-06 14:03:54 +0200

    Add hero and villain class

--- src/components/Game/metaHumanGame.jsx
+++ src/components/Game/metaHumanGame.jsx
@@ -12,31 +12,57 @@ class MetaHumanGame extends React.Component {
         super(props);
 
         const characters = [
-            new Hero('Logan', 100, 4, 'Claws'),
-            new Hero('Spider-man', 80, 2, 'Web'),
-            new Villain('Magneto', 60, 8, 'Magnet'),
-            new Villain('Thanos', 99, 14, 'Infinite glove'),
+            new Hero('Logan', 100, 4, 'claws', 'gift1', 'ability1'),
+            // new Hero('Spider-man', 80, 2, 'web', 'gift2', 'ability2'),
+            new Villain('Magneto', 60, 8, 'magnet', 'curse1', 'special1'),
+            // new Villain('Thanos', 99, 14, 'infinite glove', 'curse2', 'special2'),
         ];
 
         this.state = { characters };
     }
 
-    componentDidMount() {
+    handleKickHero = () => {
+      const { characters } = this.state;
+      const [hero, villain] = characters;
+      hero.getDamage(villain.destroyWorld());
+      this.setState({ characters });
+    };
 
-    }
+    handleKickVillain = () => {
+      const { characters } = this.state;
+      const [hero, villain] = characters;
+      villain.getDamage(hero.saveWorld());
+      this.setState({ characters });
+    };
 
     render() {
         const { characters } = this.state;
+        const [hero, villain] = characters;
 
         return (
             <div style={{ width: '70%' }}>
                 <div className="App-title">Meta human battle</div>
                 <div className="App-content">
-                    {characters.map(character => (
-                        <div>
-                            <div>{character.greet()}</div>
-                        </div>
-                    ))}
+                  <div>
+                    <div>{hero.greet()}</div>
+                    <div>Health: {hero._health}</div>
+                    <button
+                      disabled={hero._isDead}
+                      onClick={this.handleKickHero}
+                    >
+                      Atack
+                    </button>
+                  </div>
+                  <div>
+                    <div>{villain.greet()}</div>
+                    <div>Health: {villain._health}</div>
+                    <button
+                      disabled={villain._isDead}
+                      onClick={this.handleKickVillain}
+                    >
+                      Atack
+                    </button>
+                  </div>
                 </div>
             </div>
         );
--- src/utils/inheritance(prototype).js
+++ src/utils/inheritance(prototype).js
@@ -3,10 +3,19 @@ function MetaHuman(name, health, kick, weapon) {
     this._health = health;
     this._kick = kick;
     this._weapon = weapon;
+    this._isDead = false;
 }
 
 MetaHuman.prototype.greet = function() {
     return `${this._name} is greeting You! And I have weapon: ${this._weapon}`;
+};
+
+MetaHuman.prototype.getDamage = function(damageValue) {
+  this._health = (this._health - damageValue).toFixed(2);
+  if(this._health <= 0) {
+    this._isDead = true;
+    this._health = 0;
+  };
 };
 
 export function Hero(name, health, kick, weapon, gift, ability) {
@@ -27,9 +36,38 @@ Hero.prototype = Object.create(MetaHuman.prototype);
 Villain.prototype = Object.create(MetaHuman.prototype);
 
 Hero.prototype.saveWorld = function() {
-    return `${this._name} save world with help of ${this._ability}`;
+    const damageValue = this._kick * weaponBoost[this._weapon] + (Hero.gifts[this._gift] * Hero.abilities[this._ability]);
+    return damageValue.toFixed(2);
 };
 
 Villain.prototype.destroyWorld = function() {
-    return `${this._name} destroy world with help of ${this._special}`;
+  const damageValue = this._kick * weaponBoost[this._weapon] + (Villain.curses[this._curse] * Villain.specials[this._special]);
+  return damageValue.toFixed(2);
+};
+
+const weaponBoost = {
+  'web': 1.3,
+  'claws': 1.5,
+  'magnet': 2,
+  'infinite glove': 9.9,
+};
+
+Hero.gifts = {
+  'gift1': 1.12,
+  'gift2': 1.08,
+};
+
+Hero.abilities = {
+  'ability1': 1.02,
+  'ability2': 1.18,
+};
+
+Villain.curses = {
+  'curse1': 1.16,
+  'curse2': 1.01,
+};
+
+Villain.specials = {
+  'special1': 1.22,
+  'special2': 1.1,
 };
